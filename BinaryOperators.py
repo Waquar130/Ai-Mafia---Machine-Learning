ac35555670bc3a1da5164844a360d8aea2fd47aa
Waquar Ziya <136261308+Waquar130@users.noreply.github.com>
2023-10-24 11:59:44 +0530

    Create BinaryOperators.py

---
+++ BinaryOperators.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+#creating sample arrays a and b
+a = np.array([[1, 2], 
+              [3, 4]])
+b = np.array([[4, 3], 
+              [2, 1]])
+
+# add arrays
+print("Array sum:\n", a + b)
+
+# multiply arrays (elementwise multiplication)
+print("Array multiplication:\n", a*b)
+
+# matrix multiplication
+print("Matrix multiplication:\n", a.dot(b))
